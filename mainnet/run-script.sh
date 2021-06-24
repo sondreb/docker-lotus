62f21a00afb32c916de9093afebc6906040d1bc0
Sondre <sondre@outlook.com>
2021-06-24 13:33:24 +0200

    Update mainnet script

--- mainnet/run-script.sh
+++ mainnet/run-script.sh
@@ -4,9 +4,10 @@
 # container at startup. Edit this file to customize the environment variables
 # controlling the infrastructure that the BCH ABC full node depends on.
 
+# Attempt to copy from home, allowing user to manually override the default configuration:
 cp /home/bitcoin/config/lotus.conf /data/lotus.conf
 
 #CMD ["bitcoind", "-conf=/home/bitcoin/bitcoin.conf", "-datadir=/data", "-disablewallet"]
 #CMD ["bitcoind", "-conf=/data/bitcoin.conf", "-datadir=/data", "-disablewallet"]
 
-bitcoind -conf=/data/lotus.conf -datadir=/data -disablewallet
+lotusd -conf=/data/lotus.conf -datadir=/data -disablewallet
