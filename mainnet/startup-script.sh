73892a36a503954cf4ca2fe20b91383bb4fc81b2
Sondre <sondre@outlook.com>
2021-06-24 13:15:16 +0200

    Minor improvements with paths

--- mainnet/startup-script.sh
+++ mainnet/startup-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+echo "Lotus Startup Script..."
+
 # This startup script becomes part of the Docker image. It is used
 # to grab the run-script from the persistant volume and run it.
 # Edit the run-script.sh if you need to change the way SLPDB runs,
 # including environment variables.
 cp /home/bitcoin/config/run-script.sh /home/bitcoin/run-script.sh
 cd /home/bitcoin
-
-
 
 # Function that runs when the docker container recieves the SIGTERM signal.
 stopBitcoind() {
